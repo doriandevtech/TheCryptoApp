5fc3f2f6ad6af0ef923eec8b92a0f3415f0e56b4
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-08-23 17:11:23 +0200

    update Api file

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -16,7 +16,18 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
+    getCryptoDetails: builder.query({
+      query: (uuid) => createRequest(`/coin/${uuid}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ uuid, timeperiod }) =>
+        createRequest(`coin/${uuid}/history?timePeriod=${timeperiod}`),
+    }),
   }),
 });
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
