f9701f23048da5704da6a17ea7f5225c41032565
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-08-23 17:10:33 +0200

    add link to crypto card

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -34,15 +34,17 @@ const Cryptocurrencies = ({ simplified }) => {
       <Row gutter={[32, 32]} classNamecrypto-card-container>
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-            <Card
-              title={`${currency.rank}. ${currency.name}`}
-              extra={<img className="crypto-image" src={currency.iconUrl} />}
-              hoverable
-            >
-              <p>Price: {millify(currency.price)}</p>
-              <p>Market Cap: {millify(currency.marketCap)}</p>
-              <p>Daily Change: {millify(currency.change)}</p>
-            </Card>
+            <Link to={`/coin/${currency.uuid}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}</p>
+              </Card>
+            </Link>
           </Col>
         ))}
       </Row>
